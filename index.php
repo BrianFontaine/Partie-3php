3d27a724aeeffbe1586387f102cd366f4af13836
Brian Fontaine <brian02440@live.fr>
2020-06-23 09:42:14 +0200

    correction

--- index.php
+++ index.php
@@ -3,10 +3,10 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Partie 9 PHP</title>
+    <title>Partie 3 PHP</title>
 </head>
 <body>
-    <h1>Partie 9 php</h1>
+    <h1>Partie 3 php</h1>
     <a href="exercice1php/index.php">Exercie 1</a>
     <a href="exercice2php/index.php">Exercie 2</a>
     <a href="exercice3php/index.php">Exercie 3</a>
